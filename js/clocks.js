41f183f8172f5fc5bcf542ad27e544b93749b6e7
Manuel Schmalstieg <git.code@ms-studio.net>
2017-10-05 08:11:58 +0200

    optimisations

--- js/clocks.js
+++ js/clocks.js
@@ -4,42 +4,68 @@
 // Cloner un élément 11 fois:
 // On va créer 12 nouveaux objets, et on va les masquer
 
-			// On sélectionner notre modèle:
-			var item = document.getElementById("batons1");
+// On sélectionne notre modèle:
+var item = document.getElementById("batons1");
 
-			var i;
-			for (i = 2; i < 13; i++) {
+var i;
+for (i = 2; i < 13; i++) {
 
-				// Copy the element
-				var clone = item.cloneNode(true);
+	// On commence à 2, car on veut numéroter de 2 à 12
 
-				// Change the elment
-				clone.id = 'batons'+i;
+	var clone = item.cloneNode(true);
+	clone.id = 'batons'+i;
+  document.querySelector("body").appendChild(clone);
 
-				// Append the cloned element to something
-				// document.getElementById("vector").appendChild(clone);
-        document.querySelector("body").appendChild(clone);
-
-			}
-
-// répéter la fonction metronone une fois par seconde
-
-var myVar = setInterval(function() {
-  metronome();
-}, 1000);
+}
 
 function metronome() {
 
   // 1: on définit l'heure, avec l'objet Date()
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
-  var d = new Date();
+  var date = new Date();
+	var seconde = date.getSeconds() + 1;
 
-  // coloriser les éléments:
-  // faire des tranches de 5 secondes = diviser la seconde par 12.
+	// Cas spécifique de la seconde zéro
+	if (seconde == 60) {
+		seconde = 1;
+		groupe = 1;
+		item = 1;
 
-  // 2: on affiche l'heure dans notre objet "horloge"
+		// Cacher tous les éléments
+		var hideAll = document.getElementsByClassName("baton") ;
+  	var i;
+  	for (i = 0; i < hideAll.length; i++) {
+			hideAll[i].style.visibility = "hidden";
+  	}
 
-  // document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
+  	// Afficher l'élément zéro:
+  	var showSec = document.querySelector("#batons1 .baton1 .st0");
+		showSec.style.visibility = "visible";
 
-}
+	} else {
+
+		// Calcul mathématique:
+		// on veut que ça aille de 1 à 12
+		// en partant de chiffres de 1 à 60:
+	  // faire des tranches de 5 secondes = diviser les secondes par 6.
+		var groupe = Math.floor( seconde / 5);
+
+		// Dans chaque groupe, égréner de 1 à 5 les secondes
+		var item = (seconde - (groupe*5));
+
+		groupe = groupe + 1;
+		item = item + 1;
+
+		var itemActif = document.querySelector("#batons"+groupe+" .baton"+item+" ");
+		itemActif.style.visibility = "visible";
+
+	}
+
+} // fin du metronome()
+
+// Répéter la fonction metronone une fois par seconde
+
+var myVar = setInterval(function() {
+  metronome();
+}, 100);
